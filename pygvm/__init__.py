2bcd0c803d9922e23b36de136c024e3cfd7b30a4
Craig de Stigter <craig.destigter@koordinates.com>
2011-10-26 11:42:18 +1300

    move members stuff from clusters into cluster add reduce(), fix a few bugs

--- pygvm/__init__.py
+++ pygvm/__init__.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 """
 Simplified port of GVM for python.
 
@@ -46,7 +45,7 @@ class Cluster(object):
         self.variance = 0.0
         self.members = []
 
-    def set(self, mass, coords):
+    def set(self, mass, coords, members):
         """
         Sets this cluster equal to a single point.
         """
@@ -54,33 +53,32 @@ class Cluster(object):
         self.m2 = [mass * coord * coord for coord in coords]
         self.mass = mass
         self.variance = 0.0
+        self.members = members
 
-    def add(self, mass, coords):
+    def add(self, mass, coords, members):
         """
         Adds a point to the cluster.
         """
         if not self.mass:
-            self.set(mass, coords)
+            self.set(mass, coords, members)
         else:
             if mass != 0:
                 self.mass += mass
                 for i, coord in enumerate(coords):
                     self.m1[i] += coord * mass
                     self.m2[i] += coord * coord * mass
+                self.members.extend(members)
                 self.update()
 
     def add_cluster(self, cluster):
         """
         Adds the specified cluster to this cluster.
         """
-        self.mass += cluster.mass
-        self.m1 = map(sum, zip(self.m1, cluster.m1))
-        self.m2 = map(sum, zip(self.m2, cluster.m2))
-        self.update()
+        self.add(cluster.mass, cluster.center(), cluster.members)
 
     def test(self, mass, coords):
         """
-        Computes this clusters variance if it were to have a new point added to it.
+        Computes the change in this clusters variance if it were to have a new point added to it.
         """
         new_mass = self.mass + mass
         if new_mass == 0:
@@ -103,7 +101,7 @@ class Cluster(object):
             return 0.0
         else:
             total = 0
-            for tm1, tm2, cm1, cm2 in zip(self.m1, self.m2, cluster.m1, cluster.m2):
+            for (tm1, tm2, cm1, cm2) in zip(self.m1, self.m2, cluster.m1, cluster.m2):
                 m1 = tm1 + cm1
                 m2 = tm2 + cm2
                 total += (m2 * new_mass) - (m1 * m1)
@@ -130,6 +128,7 @@ class ClusterPair(object):
         self.c1 = c1
         self.c2 = c2
         self.value = None
+        self.update()
 
     def __lt__(self, other):
         # not a typo! makes the heap algorithm have large stuff first
@@ -175,19 +174,21 @@ class Clusters(object):
         self.clusters = []
         self.pairs = {}
 
+    def create_cluster(self):
+        return Cluster(self)
+
     def add(self, mass, coords, members):
         if mass == 0:
             return
         if len(self.clusters) < self.capacity:
-            cluster = Cluster(self)
+            cluster = self.create_cluster()
             self.clusters.append(cluster)
-            cluster.set(mass, coords)
+            cluster.set(mass, coords, members)
             self._add_pairs()
-            cluster.members = members
         else:
             #identify cheapest merge
             merge_pair = self.pairs.peek()
-            merge_t = merge_pair.value if merge_pair else MAX_FLOAT
+            merge_t = merge_pair and merge_pair.value or MAX_FLOAT
 
             # find cheapest addition
             addition_c = None
@@ -200,21 +201,61 @@ class Clusters(object):
 
             if addition_t <= merge_t:
                 # chose addition
-                addition_c.add(mass, coords)
+                addition_c.add(mass, coords, members)
                 self._update_pairs(addition_c)
-                addition_c.members.extend(members)
             else:
                 # choose merge
                 c1 = merge_pair.c1
                 c2 = merge_pair.c2
                 if c1.mass < c2.mass:
                     (c1, c2) = (c2, c1)
-                c1.members.extend(c2.members)
                 c1.add_cluster(c2)
                 c2.members = []
 
     def results(self):
         return self.clusters[:]
+
+    def reduce(self, max_variance, min_clusters):
+        if len(self.clusters) <= min_clusters:
+            return
+
+        count = len(self.clusters)
+        total_variance = sum([c.variance for c in self.clusters])
+        total_mass = sum([c.mass for c in self.clusters])
+
+        while count > min_clusters:
+            if count == 1:
+                for c in self.clusters:
+                    if not c.removed:
+                        c.removed = True
+            else:
+                merge_pair = self.pairs.peek()
+                c1 = merge_pair.c1
+                c2 = merge_pair.c2
+                if c1.mass < c2.mass:
+                    c1, c2 = c2, c1
+                if max_variance >= 0:
+                    diff = c1.test_cluster(c2) - c1.variance - c2.variance
+                    total_variance += diff
+                    if total_variance / total_mass > max_variance:
+                        # stop here, we are going to exceed maximum
+                        break
+                c1.add_cluster(c2)
+                self._update_pairs(c1)
+                self._remove_pairs(c2)
+                c2.removed = True
+            count -= 1
+
+        # remove dead clusters
+        for i, c in reversed(list(enumerate(self.clusters))):
+            if c.removed:
+                self.clusters.pop(i)
+
+        # remove dead pairs
+        for c in self.clusters:
+            for i, pair in reversed(list(enumerate(c.pairs))):
+                if pair.c1.removed or pair.c2.removed:
+                    c.pairs.pop(i)
 
     def _add_pairs(self):
         cj = self.clusters[-1]
@@ -224,6 +265,7 @@ class Clusters(object):
             pair = ClusterPair(ci, cj)
             ci.pairs.append(pair)
             cj.pairs.append(pair)
+            self.pairs.add(pair)
 
     def _update_pairs(self, cluster):
         pairs = cluster.pairs
@@ -231,4 +273,12 @@ class Clusters(object):
         for i, pair in enumerate(pairs):
             if i >= limit:
                 break
+            if pair.c1.removed or pair.c2.removed:
+                continue
             self.pairs.reprioritize(pair)
+
+    def _remove_pairs(self, cluster):
+        for pair in cluster.pairs:
+            if pair.c1.removed or pair.c2.removed:
+                continue
+            self.pairs.remove(pair)
